758961bbe42097857e9538a0d9d7c9cf971a60de
Luis <luis.fgbo@gmail.com>
2023-08-02 00:00:42 +0100

    feat(screenshots): initial composition tests

--- src/utils/screenshot.ts
+++ src/utils/screenshot.ts
@@ -1,4 +1,5 @@
 import { chromium } from 'playwright';
+import sharp from 'sharp';
 
 // TODO: create some kind of file naming strategy
 function getNameFromUrl(url: string) {
@@ -15,6 +16,8 @@ async function takeScreenshot(
 ) {
   const browser = await chromium.launch();
   const context = await browser.newContext({
+    locale: 'en-GB',
+    timezoneId: 'Europe/London',
     deviceScaleFactor,
     isMobile: deviceName.includes('iphone') || deviceName.includes('ipad'),
     hasTouch: deviceName.includes('iphone') || deviceName.includes('ipad'),
@@ -30,12 +33,14 @@ async function takeScreenshot(
   // wait for animations to finish
   await page.waitForTimeout(3000);
 
-  const selector = 'a[id*=cookie i], a[class*=cookie i], button[id*=cookie i] , button[class*=cookie i]';
-  const expectedCookieAcceptText = /^(Accept|Accept all cookies|Accept all|Allow|Allow all|Allow all cookies|OK)$/gi;
-  const cookieAcceptElements = await page.locator(selector);
+  const selector =
+    'a[id*=cookie i], a[class*=cookie i], button[id*=cookie i] , button[class*=cookie i]';
+  const expectedCookieAcceptText =
+    /^(Accept|Accept all cookies|Accept all|Allow|Allow all|Allow all cookies|OK)$/gi;
+  const cookieAcceptElements = page.locator(selector);
   const count = await cookieAcceptElements.count();
   for (let i = 0; i < count; i++) {
-    const cookieAcceptElement = await cookieAcceptElements.nth(i);
+    const cookieAcceptElement = cookieAcceptElements.nth(i);
     const textContent = await cookieAcceptElement.textContent();
     if (expectedCookieAcceptText.test(textContent?.trim() ?? '')) {
       if (await cookieAcceptElement.isVisible()) {
@@ -48,18 +53,31 @@ async function takeScreenshot(
   // wait for banner to fade out
   await page.waitForTimeout(2000);
 
-  await page.screenshot({
+  const img = await page.screenshot({
     path: `screenshots/${getNameFromUrl(pageUrl)}-${deviceName}.png`,
   });
 
   await browser.close();
+
+  await composeImage(img);
+}
+
+export async function composeImage(img: Buffer) {
+  // const rotated = await sharp(img).rotate(10).toBuffer();
+
+  // TODO: think of a layer for the glassy look???
+  await sharp('templates/macbook-pro-2888x1908.png')
+    .composite([{ input: img, top: 140, left: 360 }])
+    .toFile('compositions/combined.png');
 }
 
 export async function takeScreenshots(pageUrl: string) {
   const screenshots = [
-    void takeScreenshot(pageUrl, 'iphone13pro', 390, 844, 3),
-    void takeScreenshot(pageUrl, 'ipadair', 768, 1024, 2),
-    void takeScreenshot(pageUrl, 'macbookproretina', 2560, 1600, 2),
+    // void takeScreenshot(pageUrl, 'iphone13pro', 390, 844, 3),
+    // void takeScreenshot(pageUrl, 'ipadair', 768, 1024, 2),
+    // void takeScreenshot(pageUrl, 'macbookproretina', 2560, 1600, 1),
+    //test
+    void takeScreenshot(pageUrl, 'macbookproretina', 2170, 1360, 1),
   ];
 
   await Promise.all(screenshots);
